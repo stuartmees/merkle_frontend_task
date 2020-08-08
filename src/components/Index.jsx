fd6cd08d2d786b5f2ed881f089a7cb98ee90d239
Stuart Mees <stuartmees@gmail.com>
2020-08-08 12:46:58 +0100

    clear results buttong added and responsive style added to index

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import IndexItem from './IndexItem';
 import getVisibleClients from '../selectors/clients'
 import setClientFilter from '../actions/filters'
 
+
 const Index = (props) => {
+    
+    const handleSetClientFilter = (value) => {
+        props.dispatch(setClientFilter(value))
+    }
 
     return(
         <div className="index">
 
             <header className="index__header">
                 <h1>Client Index</h1>
+                {props.filters.searchTerm && <button onClick={() => handleSetClientFilter('')}>Clear Search</button>}   
                 <input 
                     type="text"
                     placeholder=" Search for your client" 
                     value={props.filters.searchTerm} 
-                    onChange={(e) => {
-                        props.dispatch(setClientFilter(e.target.value))
-                    }} 
+                    onChange={(e) => handleSetClientFilter(e.target.value)} 
                 />
             </header>
 
             {props.clients.map(client => <IndexItem client={client} key={client.id}/>)}
 
-            {props.clients.length===0 && props.filters.searchTerm && <h2 className="index__no-clients">Sorry, no clients match your search term.</h2>}
+            {props.clients.length===0 && props.filters.searchTerm && <div className="index__no-clients"><p>Sorry, no clients match your search term.</p></div>}
+
         </div>
     )
 
