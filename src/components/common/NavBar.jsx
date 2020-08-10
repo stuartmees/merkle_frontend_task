cd826c12564d278c0a5f06bf540d5d2cb4db87db
Stuart Mees <stuartmees@gmail.com>
2020-08-10 16:33:05 +0100

    nav search added

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import SearchForm from '../common/SearchForm'
+import setClientFilter from '../../actions/filters'
 
 class NavBar extends React.Component {
 
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
+            value: ''
         }
-      }
+    }
 
-    render() {
-        return(
-        <nav className="nav-bar">
-            <Link to={`/`}>
-                <h1>Merkle</h1>
-            </Link>
-        </nav>
-        ) 
+        handleSubmit = (e) => {
+            e.preventDefault()
+            console.log('submitted')
+            // console.log(props.filters)
+            console.log(this.state.value)
+            this.props.dispatch(setClientFilter(this.state.value))
+        }
+
+        handleChange = (value) => {
+            console.log('change')
+            console.log(value)
+            this.setState({value})
+        }
+
+        render() {
+            return(
+                <nav className="nav-bar">
+                    <Link to={`/`}>
+                        <h1>Merkle</h1>
+                    </Link>
+                    <SearchForm onChange={(e) => this.handleChange(e.target.value)} value={this.state.value} onSubmit={(e) => this.handleSubmit(e)} />
+                </nav>
+            )
+        }
+ 
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        filters: state.filters,
     }
 }
 
-export default NavBar
+export default connect(mapStateToProps)(NavBar);
